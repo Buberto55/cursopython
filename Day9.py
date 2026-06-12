301aec172f1460036378d68caf06c15f6d91bfd0
Buberto <jannu55@hotmail.com>
2026-06-12 16:02:49 -0600

    Empezamos dia 10

--- Day9.py
+++ Day9.py
@@ -15,7 +15,7 @@ prog_dic["Bug"] = "Una pelucita en tu compu"
 # Clear
 empty_dictionary = {}
 
-#prog_dic = {}
+prog_dic = {}
 
 print(prog_dic)
 
@@ -50,12 +50,51 @@ print(student_grades)
 # Nesting
 # we can have a dictionary and a list inside a dictionary
 travel_log = {
-    "France": ["Paris", "Lille", "Dijon-"],
-    "Germany": ["Stuttgart", "Berlin"]
+    #"France": ["Paris", "Lille", "Dijon-"],
+    #"Germany": ["Stuttgart", "Berlin"]
+    "France" : {
+        "num_times_visited": 8,
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits" : 5
+    },
 }
 
-print(travel_log["France"][1])
+# print(travel_log["France"][1])
 
-nested_list = ["A", "B", ["C", "D"]]
+# nested_list = ["A", "B", ["C", "D"]]
 
-print(nested_list[2][1])
+# print(nested_list[2][1])
+
+print(travel_log["Germany"]["cities_visited"][2])
+
+# Project: The secret audition program instructions
+import os
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+limpiar_pantalla()
+
+subasta = {}
+seguimos = True
+
+print("Bienvenido a la super subasta de bubu!")
+print("quien sera capaz de llevarse a este papucho inutil?")
+print("\n")
+
+while seguimos:
+    nombre = input("Como te llamas? ")
+    pago = int(input("Cual es tu subasta: $"))
+    subasta[nombre] = pago
+    opc = input("Alguien mas va a subastar? si o no? ").lower()
+    if opc == "no":
+        seguimos = False
+        limpiar_pantalla()
+    elif opc == "si":
+        limpiar_pantalla()
+        print("Otra subasta!")
+print("El ganador es:",max(subasta,key=subasta.get),
+      "Con una subasta de:","$"+str(max(subasta.values())))
