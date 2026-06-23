d19c744b258212404a089371138ce3802600bd1c
Buberto <jannu55@hotmail.com>
2026-06-23 17:19:11 -0600

    Empezamos a hacer Higher Lower!!!

---
+++ Day014.py
@@ -0,0 +1,6 @@
+from Day014_Data import data
+
+print("Jugemos aaa.... Higher Lower!!!!")
+
+game = str(input("Quieres jugar?: 'Si' o 'No': ")).lower()
+
---
+++ Day014_Data.py
@@ -0,0 +1,50 @@
+data = [
+    {
+        'name' : 'Instagram',
+        'follower_count' : 346,
+        'descripion' : 'Social media plataform',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Cristiano Ronaldo',
+        'follower_count' : 215,
+        'descripion' : 'Footballer',
+        'country' : 'Portugal'
+    },
+    {
+        'name' : 'Ariana Grande',
+        'follower_count' : 183,
+        'descripion' : 'Mosician and actrees',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Dwayne Johnson',
+        'follower_count' : 181,
+        'descripion' : 'Actor and professional wrestler',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Metallica',
+        'follower_count' : 267,
+        'descripion' : 'Music group',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Pink Floid',
+        'follower_count' : 290,
+        'descripion' : 'Music group',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Minecraft',
+        'follower_count' : 330,
+        'descripion' : 'Videogame',
+        'country' : 'United States'
+    },
+    {
+        'name' : 'Clash Royale',
+        'follower_count' : 206,
+        'descripion' : 'Videogame',
+        'country' : 'Finland'
+    }
+]
