0f65c5972bfc863a7b0c57a3caba5560f4ccd70d
Buberto <jannu55@hotmail.com>
2026-07-01 19:41:23 -0600

    vamos vamos

--- Day014.py
+++ Day014.py
@@ -1,6 +1,30 @@
 from Day014_Data import data
+import random
 
 print("Jugemos aaa.... Higher Lower!!!!")
 
-game = str(input("Quieres jugar?: 'Si' o 'No': ")).lower()
+def getrandomdata(act):
+    global data
+    print(act)
+    acum = random.choice(data)
+    print(acum)
+    while act == acum:
+        print(acum, act, "Se repitio!")
+        acum = random.choice(data)
+        print(acum, act, "El nuevo")
+
+    return acum
+
+val1 = {}
+val2 = {}
+
+val1 = getrandomdata(val1)
+
+print(f"compara: \n{val1}\n{val2}\ntantan")
+
+val2 = getrandomdata(val1)
+
+print(f"compara: \n{val1}\n{val2}\ntantan")
+
+#game = str(input("Quieres jugar?: 'Si' o 'No': ")).lower()
 
